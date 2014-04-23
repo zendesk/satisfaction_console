134ccdc93fd0b90a7d284ffd083a130477bdfc9f
Joseph <joe@josephmccarron.com>
2014-04-23 15:53:39 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+:warning: *Use of this software is subject to important terms and conditions as set forth in the License file* :warning:
+
+# Satisfaction Console App
+A full-page proof-of-concept app for pulling ticket satisfaction ratings from the past x days and providing a ticket export CSV via data-uri.
---
+++ app.js
@@ -0,0 +1,206 @@
+(function() {
+  return {
+    defaultState: 'loading',
+    events: {
+      'app.activated':'getDate',
+      'click .submit':'loadChoices',
+      'click .show_form': function(e) {
+        if (e) { e.preventDefault(); }
+        this.switchTo('form');
+      },
+      //request events
+      'getFilteredRatings.done':'parseSatRatings',
+      'getAllRatings.done':'parseSatRatings'
+    },
+    requests: {
+      getFilteredRatings: function(filter, next_page_url) {
+        if (next_page_url) {
+          return { url: next_page_url + '&score=' + filter };
+        } else {
+          return { url: '/api/v2/satisfaction_ratings.json?&sort_order=desc&score=' + filter };
+        }
+        
+      },
+      //not strictly necessary
+      getAllRatings: function(filter, next_page_url) {
+        if (next_page_url) {
+          return { url: next_page_url };
+        } else {
+          return { url: '/api/v2/satisfaction_ratings.json?sort_order=desc' };
+        }
+      },
+      getUser: function(id, n, role) {
+        return {
+          url: '/api/v2/users/' + id + '.json',
+          role: 'assignee',
+          success: function(data){this.addUserName(data, n, role);}
+        };
+      },
+      getOrg: function(id, n) {
+        return {
+          url: '/api/v2/organizations/' + id + '.json',
+          success: function(data){this.addOrgName(data, n);}
+        };
+      }
+    },
+    //NAMED FUNCTIONS
+    getDate: function() {
+      var t = new Date();
+      var d = t.getDate();
+      var m = t.getMonth()+1;
+      var y = t.getFullYear();
+      if (d < 10){
+        d = '0' + d;
+      }
+      if(m < 10){
+        m = '0' + m;
+      }
+      this.datePretty = m + '/' + d + '/' + y;
+      this.dateMs = Date.now();
+      //console.log("1. Get the Date: " + this.dateMs);
+      this.loadSettings();
+    },
+    loadSettings: function() {
+      var filter = this.setting('Default Filter'),
+          autoLoad = this.setting('Auto Load');
+      this.daysBack = this.setting('Days Back');
+      if (autoLoad===true) {
+        this.loadRatings(filter);
+      } else{
+        this.switchTo('form');
+      }
+    },
+    loadChoices: function(e) {
+      if (e) { e.preventDefault(); }
+      var filter = this.$('#filter_select').val();
+      this.daysBack = this.$('#range').val();
+      this.loadRatings(filter);
+      this.switchTo('loading');
+    },
+    loadRatings: function(filter, next_page_url) {
+      if (filter == 'received' || filter == 'received_with_comment' || filter == 'received_without_comment' || filter == 'good' || filter == 'good_with_comment' || filter == 'good_without_comment' || filter == 'bad' || filter == 'bad_with_comment' || filter == 'bad_without_comment') {
+        this.filter = filter;
+        if (next_page_url) {
+          //console.log("Loading next page of ratings...\n ...w/ filter: " + filter + "\n  ...and url: " + next_page_url);
+          this.ajax('getFilteredRatings', filter, next_page_url);
+        } else {
+          //console.log("Loading ratings...\n ...w/ filter: " + filter + "\n  ...and no url(?) " + next_page_url);
+          this.ajax('getFilteredRatings', filter);
+        }
+        //this conditional isn't stricly necessary. it restricts the parameter value,
+        // but so does the API and it doesn't complain. plus the UI restricts it.
+      } else{
+        if (next_page_url) {
+          this.filter = 'none';
+          //console.log("Loading next page of ratings...\n ...w/o a filter");
+          this.ajax('getAllRatings', next_page_url);
+        } else {
+          this.filter = 'none';
+          //console.log("Loading ratings...\n ...w/o a filter");
+          this.ajax('getAllRatings');
+        }
+      }
+    },
+    parseSatRatings: function(data) {
+      var ratings = data.satisfaction_ratings;
+      this.startDate = this.dateMs - (this.daysBack * 86400000);
+      //check the date of the last rating on the page
+      this.lastRatingMs = Date.parse(ratings[ratings.length - 1].created_at);
+      //console.log(this.lastRatingMs);
+      if (data.previous_page===null && data.next_page) {
+        this.ratings = ratings;
+        this.loadRatings(this.filter, data.next_page);
+      } else if (data.previous_page && data.next_page && this.lastRatingMs > this.startDate) {
+        this.ratings = this.ratings.concat(ratings);
+        this.loadRatings(this.filter, data.next_page);
+      } else if (data.previous_page===null && data.next_page===null) {
+        this.ratings = ratings;
+      } else {
+        this.ratings = this.ratings.concat(ratings);
+        //console.log("Stopping load at a last rating of " + this.lastRatingMs + " ms \/ " + this.startDate + " start date");
+        this.encodeRatings();
+      }
+    },
+    encodeRatings: function() {
+      var n = 0,
+        ratingMs = Date.now();
+        //console.log("Now: " + ratingMs);
+      this.user_i = 0;
+      this.unencoded = [];
+      this.encoded = [];
+      while (ratingMs > this.startDate) {
+        this.unencoded[n] = this.ratings[n];
+        //format date
+        var locale_string = new Date(this.ratings[n].created_at);
+        this.unencoded[n].created_at = locale_string.toLocaleDateString();
+        //add thumb
+        if(this.ratings[n].score == 'good') {
+          this.unencoded[n].thumb = '<i class="icon-thumbs-up"></i>';
+          this.unencoded[n].score = helpers.fmt("<span class='label label-success'>%@</span>", this.unencoded[n].score);
+        } else if (this.ratings[n].score == 'bad') {
+          this.unencoded[n].thumb = '<i class="icon-thumbs-down"></i>';
+          this.unencoded[n].score = helpers.fmt("<span class='label label-important'>%@</span>", this.unencoded[n].score);
+        }
+        this.encoded[n] = {
+          ticket_id: encodeURIComponent(this.ratings[n].ticket_id),
+          score: encodeURIComponent(this.ratings[n].score),
+          organization_id: encodeURIComponent(this.ratings[n].organization_id),
+          assignee_id: encodeURIComponent(this.ratings[n].assignee_id),
+          created_at: encodeURIComponent(this.ratings[n].created_at),
+          comment: encodeURIComponent(this.ratings[n].comment)
+        };
+        console.log(this.ratings[n].assignee_id);
+        console.log(this.user_i);
+        if (this.ratings[n].assignee_id) {
+          this.ajax('getUser', this.ratings[n].assignee_id, n, 'assignee');
+        } else {
+          this.user_i++;
+        }
+        //this.ajax('getOrg', this.ratings[n].organization_id, n);
+        ratingMs = Date.parse(this.ratings[n].created_at);
+        //console.log("Next one created at: " + this.ratings[n].created_at + ", " + ratingMs);
+        n++;
+      }
+      console.log(this.unencoded);
+      // this.switchTo('csv', {
+      //   ratings: this.unencoded,
+      //   encoded_ratings: this.encoded,
+      //   filter: this.filter,
+      //   daysBack: this.daysBack
+      // });
+      
+    },
+    addUserName: function(data, n, role) {
+      this.user_i++;
+      console.log(this.user_i);
+      var user = data.user,
+        userName = user.name;
+        // console.log(userName);
+        this.unencoded[n].assignee = userName;
+        this.encoded[n].assignee = encodeURIComponent(userName);
+        //this.unencoded[n].requester = userName;
+        //this.encoded[n].requester = encodeURIComponent(userName);
+      if(this.user_i == this.unencoded.length) {
+        this.switchTo('csv', {
+          ratings: this.unencoded,
+          encoded_ratings: this.encoded,
+          filter: this.filter,
+          daysBack: this.daysBack
+        });
+      }
+    },
+    // addOrgName: function(data, n) {
+    //   var org = data.organization;
+    //     orgName = org.name;
+    //     this.unencoded[n].organization = orgName;
+    //     this.encoded[n].organization = encodeURIComponent(orgName);
+    //   this.switchTo('csv', {
+    //     ratings: this.unencoded,
+    //     encoded_ratings: this.encoded,
+    //     filter: this.filter,
+    //     daysBack: this.daysBack
+    //   });
+    // }
+  };
+
+}());
